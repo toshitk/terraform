8a18f8d954094bd3d382d19c8b8944903985787b
toshitk <takahashi.t66@gmail.com>
2024-02-01 15:11:25 +0900

    feat: add data transer setting

--- bigquery/main.tf
+++ bigquery/main.tf
@@ -2,6 +2,14 @@ variable "project_id" {
   description = "The Google Cloud project ID"
   type        = string
 }
+variable "s3_access_key" {
+  description = "Amazon S3 Access Key"
+  type        = string
+}
+variable "s3_secret_key" {
+  description = "Amazon S3 Secret Key"
+  type        = string
+}
 
 provider "google" {
   project = var.project_id
@@ -18,5 +26,22 @@ resource "google_bigquery_data_transfer_config" "scheduled_query" {
     query                 = "select * from dataset.sample limit 1;"
     destination_table_name_template = "tf_scheduled_query1_{run_date}"
     write_disposition     = "WRITE_TRUNCATE"
+  }
+}
+
+resource "google_bigquery_data_transfer_config" "s3_data_transfer" {
+  destination_dataset_id = "data_transfer_test"
+  display_name           = "tf_from_s3"
+  data_source_id         = "amazon_s3"
+  schedule               = "every 24 hours"
+  location               = "asia-northeast1"
+
+  params = {
+    destination_table_name_template = "tf_users"
+    file_format                     = "JSON"
+    max_bad_records                 = "0"
+    data_path                       = "s3://test-bq-data-transfer/transfer-data.json"
+    access_key_id                   = var.s3_access_key
+    secret_access_key               = var.s3_secret_key
   }
 }
